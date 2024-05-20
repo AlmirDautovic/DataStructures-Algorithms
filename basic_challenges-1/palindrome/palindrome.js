e227896ea6cbe505a94c16726598de23ce769a86
AlmirDautovic <aalmir.d@gmail.com>
2024-05-20 23:21:52 +0200

    solved problem another way, where i had more helping fuinctions, one to remove non alphanumeric character in way to check character codes to make sure they are in range for only numbers and lower cased letters and reverse string and compare to original str

--- basic_challenges-1/palindrome/palindrome.js
+++ basic_challenges-1/palindrome/palindrome.js
@@ -1,9 +1,49 @@
+// function isPalindrome(str) {
+//     const formatedStr = str.toLowerCase().replace(/[^a-z0-9]/g, '');
+
+//     const reversedStr = formatedStr.split('').reverse().join('');
+
+//     return formatedStr === reversedStr;
+// }
+
 function isPalindrome(str) {
-    const formatedStr = str.toLowerCase().replace(/[^a-z0-9]/g, '');
+    const formattedStr = removeNonAlphanumeric(str.toLowerCase());
 
-    const reversedStr = formatedStr.split('').reverse().join('');
+    const reversedStr = reverseStr(formattedStr);
 
-    return formatedStr === reversedStr;
+    return formattedStr === reversedStr;
+}
+
+
+function removeNonAlphanumeric(str) {
+    let formattedStr = '';
+
+    for (let i = 0; i < str.length; i++) {
+        const char = str[i];
+
+        if (isAlpanumeric(char)) {
+            formattedStr += char;
+        }
+    }
+    return formattedStr;
+}
+
+function isAlpanumeric(char) {
+    const code = char.charCodeAt(0);
+
+    return (
+        (code >= 48 && code <= 57) || (code >= 97 && code <= 122)
+    );
+}
+
+function reverseStr(str) {
+    let reversed = '';
+
+    for (let i = str.length - 1; i >= 0; i--) {
+        reversed += str[i]
+    }
+
+    return reversed;
 }
 
 
