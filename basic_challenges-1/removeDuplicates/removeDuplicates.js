c910915f3608bc9036425a31d8244a33586b2e2e
AlmirDautovic <aalmir.d@gmail.com>
2024-05-23 23:53:15 +0200

    solved another problem where it was needed to remove duplicate from array. wrote tests too that passed

---
+++ basic_challenges-1/removeDuplicates/removeDuplicates.js
@@ -0,0 +1,14 @@
+function removeDuplicates(arr) {
+    let unique = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        if (!unique.includes(arr[i])) {
+            unique.push(arr[i]);
+        }
+    }
+
+    return unique;
+}
+
+
+module.exports = removeDuplicates;
---
+++ basic_challenges-1/removeDuplicates/removeDuplicates_run.js
@@ -0,0 +1,5 @@
+const removeDuplicates = require('./removeDuplicates');
+
+const result = removeDuplicates([1, 2, 3, 2, 4, 1, 5]);
+
+console.log(result);
