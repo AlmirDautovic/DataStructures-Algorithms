6efee4e988bd6e6f1b2e07ebf10760345067b156
AlmirDautovic <aalmir.d@gmail.com>
2024-06-14 23:32:40 +0200

    added another sollution

--- basic_challenges_2/firstNonRepeating/firstNonRepeatingChar.js
+++ basic_challenges_2/firstNonRepeating/firstNonRepeatingChar.js
@@ -15,4 +15,26 @@ function findFirstNonRepeatingChar(str) {
     return null;
 }
 
+// Solution 2
+function findFirstNonRepeatingCharacter(str) {
+    // Create a new map
+    const charCount = new Map();
+
+    // Loop through the string
+    for (const char of str) {
+        // If the map already has the current character, increment the count
+        charCount.set(char, (charCount.get(char) || 0) + 1);
+    }
+
+    // Loop through the string
+    for (const char of str) {
+        // If the count of the current character is 1, return the current character
+        if (charCount.get(char) === 1) {
+            return char;
+        }
+    }
+
+    // If no characters are repeated, return null
+    return null;
+}
 module.exports = findFirstNonRepeatingChar;
